6a1394f2c1377ebb9777969bcacb3b199316ba97
BrettLanoux <97562573+BrettLanoux@users.noreply.github.com>
2022-10-14 10:31:53 -0500

    Update clickingAnimation.js

--- clickingAnimation.js
+++ clickingAnimation.js
@@ -1,4 +1,5 @@
 var mC2 = document.getElementById('mainClickingTwo');
+var bmC = document.getElementById('BrokenClicking');
 
 var mouseStatus = 'up';
 var mouseTimeout;
@@ -11,8 +12,8 @@ mC.addEventListener("mousedown",function() {
     moneyAmount++
     mouseTimeout = setTimeout(function(){
         mouseStatus='longDown';
-        //doSpecialStuffBecauseOfLongDown(); // put your secret sauce here
-    }, 1500);
+        doSpecialStuffBecauseOfLongDown(); // put your secret sauce here
+    }, 3000);
 }, false);
 mC2.addEventListener("mouseup",function() {
     clearTimeout(mouseTimeout);
@@ -20,3 +21,16 @@ mC2.addEventListener("mouseup",function() {
     mC2.style.display = 'none';
     mouseStatus='up';
 }, false);
+
+bmC.addEventListener("mouseup", function() {
+    clearTimeout(mouseTimeout);
+    mC.style.display = 'initial';
+    mC2.style.display = 'none';
+    bmC.style.display = 'none';
+    mouseStatus='up';
+}, false);
+
+function doSpecialStuffBecauseOfLongDown() {
+    mC2.style.display = 'none';
+    bmC.style.display = 'initial';
+}
